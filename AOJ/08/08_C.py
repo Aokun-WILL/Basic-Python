e5c9b0da2a405b1f0243313e488f70eee654970d
Aokun-WILL <107824782+Aokun-WILL@users.noreply.github.com>
2022-10-04 23:45:02 +0900

    Update 08_C.py

--- AOJ/08/08_C.py
+++ AOJ/08/08_C.py
@@ -1,8 +1,7 @@
 import sys
-texts = sys.stdin.read()
-texts = sys.lower()
+texts = sys.stdin.read().lower()
 letters = {chr(i): 0 for i in range(ord('a'), ord('z') + 1)}
 for i in texts:
     if ord(i) in range(ord('a'), ord('z') + 1):
         letters[i] += 1
-[print(f'{k} : {v}') for k, v in letters.item()]
+[print(f'{k} : {v}') for k, v in letters.items()]
